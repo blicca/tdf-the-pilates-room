26400b84a428efb8cb26450a1afc59e48af958b5
blicca <burakbarbak@hotmail.com>
2020-06-22 14:17:53 +0300

    gitpush

--- header.php
+++ header.php
@@ -36,17 +36,6 @@ if ( is_single() ) {
 	 */
 	wp_head(); 
 	?>
-	<!-- Pilates GOOGLE ANALYTICS CODE -->
-	<!-- Global site tag (gtag.js) - Google Analytics -->	
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-167746660-1"></script>
-	<script>
-	window.dataLayer = window.dataLayer || [];
-	function gtag(){dataLayer.push(arguments);}
-	gtag('js', new Date());
-
-	gtag('config', 'UA-167746660-1');
-	</script>
-	<!-- Pilates GOOGLE ANALYTICS CODE -->
 </head>
 
 <body <?php body_class(); ?>>
