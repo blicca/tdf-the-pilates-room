f90b5706862c9e980c914080f0864b34ab65ab7e
blicca <burakbarbak@hotmail.com>
2020-06-24 20:32:44 +0300

    fdsfsd

--- template-parts/home-about.php
+++ template-parts/home-about.php
@@ -5,10 +5,10 @@ if( $hero_section ): ?>
 <section class="home-about" style="background-image:url('<?php echo esc_attr( $hero_section['hero_image'] ); ?>')">
     <div class="home-about-container theme-row">
         <div class="hero-about-content">
-            <h1><?php echo esc_attr( $hero_section['hero_title'] ); ?></h1>
-            <?php echo esc_attr( $hero_section['hero_description'] ); ?>
+            <h1><?php echo $hero_section['hero_title']; ?></h1>
+            <?php echo $hero_section['hero_description']; ?>
             <a href="#">Start 14-Day free trial</a>
-            <div class="button-desc"><?php echo esc_attr( $hero_section['button_desc'] ); ?></div>
+            <div class="button-desc"><?php echo $hero_section['button_desc']; ?></div>
         </div>
         <div class="clearfix"></div>
     </div>
