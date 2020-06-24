eb5940f93facdb29a7e1278fa0810d492cdc99d1
blicca <burakbarbak@hotmail.com>
2020-06-24 22:29:22 +0300

    sdfds

--- template-parts/home-about.php
+++ template-parts/home-about.php
@@ -2,7 +2,8 @@
 $hero_section = get_field('hero_section');
 if( $hero_section ): ?>
 
-<section class="home-about" style="background-image:url('<?php echo esc_attr( $hero_section['hero_image'] ); ?>')">
+<section class="home-about">
+    <div class="hero-bg-col" style="background-image:url('<?php echo esc_attr( $hero_section['hero_image'] ); ?>')"></div>
     <div class="home-about-container theme-row">
         <div class="hero-about-content">
             <h1><?php echo $hero_section['hero_title']; ?></h1>
@@ -11,7 +12,7 @@ if( $hero_section ): ?>
             <div class="button-desc"><?php echo $hero_section['button_desc']; ?></div>
         </div>
         <div class="clearfix"></div>
-    </div>
+    </div>    
 </section>
 
 <?php endif; ?>
