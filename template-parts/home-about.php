28287077b237c23db34a64f9182c142e5045a180
blicca <burakbarbak@hotmail.com>
2020-06-24 20:31:17 +0300

    about

--- template-parts/home-about.php
+++ template-parts/home-about.php
@@ -1,14 +1,14 @@
 <?php
-$hero = get_field('hero_section');
+$hero_section = get_field('hero_section');
 if( $hero_section ): ?>
 
-<section class="home-about" style="background-image:url('<?php echo esc_attr( $hero['hero_image'] ); ?>')">
+<section class="home-about" style="background-image:url('<?php echo esc_attr( $hero_section['hero_image'] ); ?>')">
     <div class="home-about-container theme-row">
         <div class="hero-about-content">
-            <h1><?php echo esc_attr( $hero['hero_title'] ); ?></h1>
-            <?php echo esc_attr( $hero['hero_description'] ); ?>
+            <h1><?php echo esc_attr( $hero_section['hero_title'] ); ?></h1>
+            <?php echo esc_attr( $hero_section['hero_description'] ); ?>
             <a href="#">Start 14-Day free trial</a>
-            <div class="button-desc"><?php echo esc_attr( $hero['button_desc'] ); ?></div>
+            <div class="button-desc"><?php echo esc_attr( $hero_section['button_desc'] ); ?></div>
         </div>
         <div class="clearfix"></div>
     </div>
